48ba76d19c89f0201c62db92551e0d643e14b7fc
Corey Frenette <corey@narwhunderful.com>
2014-02-14 14:47:02 -0400

    Test

--- client/src/malachite/TestGUI.java
+++ client/src/malachite/TestGUI.java
@@ -2,16 +2,23 @@ package malachite;
 
 import malachite.engine.gfx.gui.AbstractGUI;
 import malachite.engine.gfx.gui.control.Label;
+import malachite.engine.gfx.gui.control.Textbox;
 
 public class TestGUI extends AbstractGUI {
   private Label _lblTest;
+  private Textbox _txtTest;
 
   @Override
   protected void load() {
     _lblTest = new Label(this);
     _lblTest.setText("This is a test");
     _lblTest.setXYWH(200, 200, 100, 20);
+
+    _txtTest = new Textbox(this);
+    _txtTest.setXYWH(200, 224, 100, 20);
+
     controls().add(_lblTest);
+    controls().add(_txtTest);
   }
 
   @Override
