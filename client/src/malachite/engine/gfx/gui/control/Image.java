e0b25a70deb5528598f7a3a1c4222a83df86d5a8
Corey Frenette <corey@narwhunderful.com>
2014-02-16 23:45:26 -0400

    Image control

---
+++ client/src/malachite/engine/gfx/gui/control/Image.java
@@ -0,0 +1,44 @@
+package malachite.engine.gfx.gui.control;
+
+import malachite.engine.gfx.AbstractContext;
+import malachite.engine.gfx.AbstractDrawable;
+import malachite.engine.gfx.gui.AbstractControl;
+import malachite.engine.gfx.gui.AbstractGUI;
+import malachite.engine.gfx.gui.ControlEvents;
+import malachite.engine.gfx.textures.Texture;
+import malachite.engine.gfx.textures.TextureBuilder;
+
+public class Image extends AbstractControl<ControlEvents> {
+  private AbstractDrawable _image;
+
+  public Image(AbstractGUI gui) {
+    super(gui);
+    _image = AbstractContext.newDrawable();
+  }
+
+  public void setTexture(Texture texture) {
+    texture.events().addLoadHandler(() -> {
+      _image.setTexture(texture);
+      _image.createQuad();
+      setWH(texture.getW(), texture.getH());
+    });
+  }
+
+  public Texture getTexture() {
+    return _image.getTexture();
+  }
+
+  @Override
+  protected void resize() {
+
+  }
+
+  @Override
+  public void draw() {
+    if(drawBegin()) {
+      _image.draw();
+    }
+
+    drawEnd();
+  }
+}
