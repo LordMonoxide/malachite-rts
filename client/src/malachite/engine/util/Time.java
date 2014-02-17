344216cb6224c6d1ea95215d067e9ca8dbb9dd77
Corey Frenette <corey@narwhunderful.com>
2014-02-17 15:21:07 -0400

    MSToTicks fn

--- client/src/malachite/engine/util/Time.java
+++ client/src/malachite/engine/util/Time.java
@@ -2,13 +2,17 @@ package malachite.engine.util;
 
 import org.lwjgl.Sys;
 
-public class Time {
+public final class Time {
   public static double get() {
     return Sys.getTime();
   }
 
   public static double HzToTicks(double hz) {
     return 1000 / hz * Sys.getTimerResolution() / 1000;
+  }
+
+  public static double MSToTicks(double ms) {
+    return ms * Sys.getTimerResolution() / 1000;
   }
 
   private Time() { }
