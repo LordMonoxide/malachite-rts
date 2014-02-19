8e15b0800566dd8a06850533c301a1918530a8a0
Corey Frenette <corey@narwhunderful.com>
2014-02-19 15:48:38 -0400

    Frame control

---
+++ client/src/malachite/engine/gfx/gui/control/Frame.java
@@ -0,0 +1,20 @@
+package malachite.engine.gfx.gui.control;
+
+import malachite.engine.gfx.AbstractContext;
+import malachite.engine.gfx.AbstractScalable;
+import malachite.engine.gfx.textures.TextureBuilder;
+
+public class Frame extends Image {
+  public Frame() {
+    AbstractScalable s = AbstractContext.newScalable();
+    s.setTexture(TextureBuilder.getInstance().getTexture("gui/textbox.png"));
+    s.setXY(-5, -5);
+    s.setSize(
+        new float[] {12, 12, 12, 12},
+        new float[] {12, 12, 12, 12},
+        25, 25, 1
+    );
+
+    _background = s;
+  }
+}
