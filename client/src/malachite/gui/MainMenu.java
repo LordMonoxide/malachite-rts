40f17d1a2c61cf600557265224ab7eb9e8002fbf
Corey Frenette <corey@narwhunderful.com>
2014-02-19 23:36:08 -0400

    Almost register window

--- client/src/malachite/gui/MainMenu.java
+++ client/src/malachite/gui/MainMenu.java
@@ -13,8 +13,13 @@ public class MainMenu extends AbstractGUI {
   private Check _chkRemember;
   private Button _btnLogin;
   private Button _btnRegister;
-
   private Frame _fraInfo;
+
+  private Window _wndRegister;
+  private Textbox _txtRegisterName;
+  private Textbox[] _txtRegisterPass = new Textbox[2];
+  private Textbox[] _txtRegisterSecurityQuestion = new Textbox[3];
+  private Textbox[] _txtRegisterSecurityAnswer = new Textbox[3];
 
   @Override
   protected void load() {
@@ -71,17 +76,63 @@ public class MainMenu extends AbstractGUI {
     _btnRegister.setY(_btnLogin.getY());
     _btnRegister.setWH(80, 20);
     _btnRegister.setText("Register...");
+    _btnRegister.events().addClickHandler(new ControlEvents.Click() {
+      @Override
+      public void click() {
+        _wndLogin.hide();
+        _wndRegister.show();
+      }
+
+      @Override public void clickDbl() { }
+    });
 
     _fraInfo = new Frame();
     _fraInfo.setX(_chkRemember.getX());
 
+    _wndRegister = new Window();
+    _wndRegister.setWH(400, 300);
+    _wndRegister.setXY((_context.getW() - _wndRegister.getW()) / 2, (_context.getH() - _wndRegister.getH()) / 2);
+    _wndRegister.setText("Register");
+    _wndRegister.hide();
+    _wndRegister.events().addResizeHandler(new ControlEvents.Resize() {
+      @Override
+      public void resize() {
+        _txtRegisterName.setW(_wndRegister.getContentW() - _txtRegisterName.getX() * 2);
+        _txtRegisterPass[0].setW(_wndRegister.getContentW() - _txtRegisterPass[0].getX() * 2);
+        _txtRegisterPass[1].setW(_wndRegister.getContentW() - _txtRegisterPass[1].getX() * 2);
+      }
+    });
+
+    _txtRegisterName = new Textbox();
+    _txtRegisterName.setXY(4, 4);
+    _txtRegisterName.setH(20);
+    _txtRegisterName.setTextPlaceholder("Username");
+
+    _txtRegisterPass[0] = new Textbox();
+    _txtRegisterPass[0].setXY(_txtRegisterName.getX(), _txtRegisterName.getY() + _txtRegisterName.getH() + 8);
+    _txtRegisterPass[0].setH(20);
+    _txtRegisterPass[0].setTextPlaceholder("Password");
+    _txtRegisterPass[0].setMasked(true);
+
+    _txtRegisterPass[1] = new Textbox();
+    _txtRegisterPass[1].setXY(_txtRegisterPass[0].getX(), _txtRegisterPass[0].getY() + _txtRegisterPass[0].getH() + 8);
+    _txtRegisterPass[1].setH(20);
+    _txtRegisterPass[1].setTextPlaceholder("Confirm Password");
+    _txtRegisterPass[1].setMasked(true);
+
     controls().add(_wndLogin);
+    controls().add(_wndRegister);
+
     _wndLogin.controls().add(_txtName);
     _wndLogin.controls().add(_txtPass);
     _wndLogin.controls().add(_chkRemember);
     _wndLogin.controls().add(_btnLogin);
     _wndLogin.controls().add(_btnRegister);
     _wndLogin.controls().add(_fraInfo);
+
+    _wndRegister.controls().add(_txtRegisterName);
+    _wndRegister.controls().add(_txtRegisterPass[0]);
+    _wndRegister.controls().add(_txtRegisterPass[1]);
 
     _txtName.setFocus(true);
   }
