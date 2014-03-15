a36f606d1fd286b0d9110a909573113409b39bcb
Corey Frenette <corey@narwhunderful.com>
2014-03-15 20:43:01 -0300

    Remove test code

--- client/client/src/malachite/Game.java
+++ client/client/src/malachite/Game.java
@@ -1,29 +1,20 @@
 package malachite;
 
-import io.netty.handler.codec.http.HttpHeaders;
-import io.netty.handler.codec.http.HttpMethod;
+import malachite.api.API;
 import malachite.engine.gfx.AbstractContext;
 import malachite.engine.gfx.ContextListenerAdapter;
 import malachite.engine.gfx.Loader;
 import malachite.engine.gfx.Manager;
 import malachite.engine.gfx.gl14.Context;
-import malachite.engine.net.http.Request;
 import malachite.gui.MainMenu;
 
-import java.net.URISyntaxException;
-
 public class Game {
-  public static void main(String... args) throws URISyntaxException {
-    Request r = new Request();
-    r.setMethod(HttpMethod.POST);
-    r.setRoute("/menu/login");
-    r.addHeader(HttpHeaders.Names.ACCEPT, "application/json");
-    r.addData("email", "corey@narwhunderful.com");
-    r.addData("password", "monoxide");
-    r.dispatch(resp -> {
+  public static void main(String... args) {
+    API.login("corey@narwhunderful.com", "monoxide", resp -> {
       System.out.println(resp.response().getStatus());
       System.out.println(resp.content());
     });
+    
     //new Game().initialize();
   }
 
