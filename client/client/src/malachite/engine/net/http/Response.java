a3ca0b55e9155422c6f838098cde16dd736f46be
Corey Frenette <corey@narwhunderful.com>
2014-03-14 23:39:38 -0300

    Fix response being prepended with null

--- client/client/src/malachite/engine/net/http/Response.java
+++ client/client/src/malachite/engine/net/http/Response.java
@@ -4,7 +4,7 @@ import io.netty.handler.codec.http.HttpResponse;
 
 public class Response {
   HttpResponse _response;
-  String _content;
+  String _content = ""; //$NON-NLS-1$
 
   public HttpResponse response() { return _response; }
   public String       content () { return _content;  }
