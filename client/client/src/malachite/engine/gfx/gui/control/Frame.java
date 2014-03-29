a47ec6336587bcb9d2ad8c1ac2e991346cdf5f7f
Corey Frenette <corey@narwhunderful.com>
2014-03-28 22:00:17 -0300

    Allow frames to have events

--- client/client/src/malachite/engine/gfx/gui/control/Frame.java
+++ client/client/src/malachite/engine/gfx/gui/control/Frame.java
@@ -6,6 +6,11 @@ import malachite.engine.gfx.textures.TextureBuilder;
 
 public class Frame extends Image {
   public Frame() {
+    super(
+      InitFlags.WITH_DEFAULT_EVENTS,
+      InitFlags.REGISTER
+    );
+    
     AbstractScalable s = AbstractContext.newScalable();
     s.setTexture(TextureBuilder.getInstance().getTexture("gui/textbox.png"));
     s.setXY(-5, -5);
