e8ad922a67d4aa07bb1a7e2c8f79a1c029748c1f
Corey Frenette <corey@narwhunderful.com>
2014-03-31 15:57:52 -0300

    Not gonna do the API/forum separation

--- http/forum/app/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'auth'], function() {
-  Route::post('/login', ['as' => 'auth.login', 'uses' => 'forum\AuthController@login']);
-});
-
-Route::get ('/login', ['as' => 'login', 'uses' => 'forum\ForumController@login']);
