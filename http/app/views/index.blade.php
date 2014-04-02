322617e2305ffb217163d4e2f9d076d461a96ed4
Corey Frenette <corey@narwhunderful.com>
2014-04-02 12:32:44 -0300

    Add basic index

---
+++ http/app/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>{{ Lang::get('app.title') }} - {{ Lang::get('index.title') }}</title>
+    {{ HTML::style('assets/css/main.css') }}
+  </head>
+  
+  <body>
+    <p>{{ HTML::linkAction('forum.index', Lang::get('forum.title')) }}</p>
+    
+    @if(Auth::check())
+      <p>{{ HTML::linkAction('auth.logout', Lang::get('app.logout')) }}</p>
+    @else
+      <p>{{ HTML::linkAction('login', Lang::get('app.login')) }}</p>
+    @endif
+  </body>
+</html>
