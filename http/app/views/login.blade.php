27cb7ca73e02a39e026911ee19ed3f3a0e91f218
Corey Frenette <corey@narwhunderful.com>
2014-04-01 23:19:47 -0300

    Better login page

--- http/app/views/login.blade.php
+++ http/app/views/login.blade.php
@@ -4,15 +4,35 @@
   <head>
     <meta charset="utf-8">
     <title>{{ Lang::get('app.title') }} - {{ Lang::get('menu.login.title') }}</title>
+    {{ HTML::style('assets/css/main.css') }}
+    {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js') }}
+    {{ HTML::script('assets/js/home.js', ['async']) }}
   </head>
   
   <body>
-    {{ Form::open(['route' => 'auth.login', 'method' => 'POST']) }}
-    {{ Form::label('email', Lang::get('menu.login.email')) }}
-    {{ Form::email('email') }}
-    {{ Form::label('password', Lang::get('menu.login.pass')) }}
-    {{ Form::password('password') }}
-    {{ Form::submit(Lang::get('menu.login.login')) }}
-    {{ Form::close() }}
+    <div class="true-center-outer">
+      <div class="true-center-inner">
+        <div id="login-div" class="true-center-content">
+          {{ Form::hidden('route_dest',     URL::previous()) }}
+          {{ Form::hidden('route_login',    URL::route('api.auth.login')) }}
+          {{ Form::hidden('route_register', URL::route('api.auth.register')) }}
+          
+          {{ Form::open(['route' => 'auth.login', 'method' => 'POST', 'id' => 'login-form', 'class' => 'pure-form pure-form-stacked']) }}
+          <legend>@lang('app.title')</legend>
+          
+          {{ Form::email('email', Input::old('email'), ['placeholder' => Lang::get('auth.email'),            'required' => 'required', 'autofocus' => 'autofocus']) }}
+          {{ Form::password('password',                ['placeholder' => Lang::get('auth.password'),         'required' => 'required']) }}
+          {{ Form::password('password_confirmation',   ['placeholder' => Lang::get('auth.password_confirm'), 'required' => 'required', 'disabled' => 'disabled', 'style' => 'display: none;']) }}
+          
+          <label>
+            {{ Form::checkbox('newaccount', null, false) }}
+            @lang('auth.newaccount')
+          </label>
+          
+          {{ Form::submit(Lang::get('auth.submit'), ['id' => 'login', 'class' => 'pure-button pure-button-primary']) }}
+          {{ Form::close() }}
+        </div>
+      </div>
+    </div>
   </body>
 </html>
---
+++ http/public/assets/js/home.js
@@ -0,0 +1,179 @@
+var popups = [];
+
+$(function() {
+  enhance();
+});
+
+function enhance() {
+  div      = $('#login-div');
+  form     = $('#login-form');
+  email    = $('input[name=email]');
+  password = $('input[name=password]');
+  password_confirmation = $('input[name=password_confirmation]');
+  submit   = $('#login');
+  newacc   = $('input[name=newaccount]');
+  
+  _method        = $('input[name=_method]');
+  route_dest     = $('input[name=route_dest]')[0].value;
+  route_login    = $('input[name=route_login]')[0].value;
+  route_register = $('input[name=route_register]')[0].value;
+  
+  // Remove hack to centre div using table style
+  div.unwrap();
+  div.unwrap();
+  
+  $(window).resize(function(ev) {
+    div.offset({left: ($(window).width() - div.width()) / 2, top: ($(window).height() - div.height()) / 2});
+    
+    popups.forEach(function(popup) {
+      popup.move();
+    });
+  });
+  
+  disableRegister();
+  submit.hide();
+  
+  $(document).keypress(function(ev) {
+    if(ev.which === 13) {
+      form.submit();
+    }
+  });
+  
+  form.submit(function(ev) {
+    if(newacc.is(':checked')) {
+      register();
+    } else {
+      login();
+    }
+    
+    ev.preventDefault();
+  });
+  
+  newacc.click(function(ev) {
+    if(newacc.is(':checked')) {
+      enableRegister();
+    } else {
+      disableRegister();
+    }
+    
+    $('input[autofocus]').focus();
+  });
+  
+  $('input[autofocus]').focus();
+  $(window).resize();
+}
+
+function register() {
+  disableForm();
+  
+  $.ajax({
+    url:  route_register,
+    type: form[0].method,
+    data: form.serialize()
+  }).done(function(data, textStatus, jqXHR) {
+    if(jqXHR.status === 200) {
+      
+      return;
+    }
+    
+    console.log('This shouldn\'t happen:');
+    console.log(jqXHR.responseJSON);
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    switch(jqXHR.status) {
+      case 409:
+        var errors = jqXHR.responseJSON;
+        for(var error in errors) {
+          createPopup(errors[error], $('input[name=' + error + ']'));
+        }
+        
+        break;
+      
+      default:
+        console.log('This shouldn\'t happen:');
+        console.log(textStatus);
+        console.log(errorThrown);
+        console.log(jqXHR.responseJSON);
+    }
+    
+    enableForm();
+  });
+}
+
+function login() {
+  disableForm();
+  
+  $.ajax({
+    url:  route_login,
+    type: form[0].method,
+    data: form.serialize()
+  }).done(function(data, textStatus, jqXHR) {
+    window.location.replace(route_dest);
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    switch(jqXHR.status) {
+      case 401:
+        
+        break;
+      
+      case 409:
+        var errors = jqXHR.responseJSON;
+        for(var error in errors) {
+          createPopup(errors[error], $('input[name=' + error + ']'));
+        }
+        
+        break;
+      
+      default:
+        console.log('This shouldn\'t happen:');
+        console.log(textStatus);
+        console.log(errorThrown);
+        console.log(jqXHR.responseJSON);
+    }
+    
+    enableForm();
+  });
+}
+
+function disableForm() {
+  email.prop('readonly', true);
+  password.prop('readonly', true);
+  password_confirmation.prop('readonly', true);
+}
+
+function enableForm() {
+  email.prop('readonly', false);
+  password.prop('readonly', false);
+  password_confirmation.prop('readonly', false);
+}
+
+function disableRegister() {
+  _method.prop('disabled', true);
+  password_confirmation.hide();
+  password_confirmation.prop('disabled', true);
+  
+  $(window).resize();
+}
+
+function enableRegister() {
+  _method.prop('disabled', false);
+  password_confirmation.show();
+  password_confirmation.prop('disabled', false);
+  
+  $(window).resize();
+}
+
+function createPopup(text, anchor) {
+  var popup = {
+    element: $('<div class="popup">' + text + '</div>'),
+    anchor: anchor,
+    move: function() {
+      var left = this.anchor.offset().left + this.anchor.outerWidth();
+      var top  = this.anchor.offset().top + (this.anchor.outerHeight() - this.element.outerHeight()) / 2;
+      this.element.offset({left: left, top: top});
+    }
+  };
+  
+  $('body').append(popup.element);
+  
+  popup.move();
+  popups.push(popup);
+}
