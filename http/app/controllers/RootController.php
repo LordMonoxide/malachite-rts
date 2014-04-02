07ad93fad4b0f9d1d287bbe6033ae13f0f4fe5a3
Corey Frenette <corey@narwhunderful.com>
2014-04-01 23:18:57 -0300

    Protect login route from people who are already logged in

--- http/app/controllers/RootController.php
+++ http/app/controllers/RootController.php
@@ -1,6 +1,10 @@
 <?php
 
 class RootController extends BaseController {
+  public function __construct() {
+    $this->beforeFilter('nauth', ['only' => ['login']]);
+  }
+  
   public function home() {
     
   }
