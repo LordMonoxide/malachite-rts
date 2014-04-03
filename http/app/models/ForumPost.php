43e6635cb450b9c38d69767b29d5219dc03558b4
Corey Frenette <corey@narwhunderful.com>
2014-04-03 09:15:58 -0300

    Order by newest

--- http/app/models/ForumPost.php
+++ http/app/models/ForumPost.php
@@ -8,4 +8,8 @@ class ForumPost extends Eloquent {
   public function author() {
     return $this->belongsTo('User', 'author_id');
   }
+  
+  public function scopeNewest($query) {
+    return $query->orderBy('created_at');
+  }
 }
