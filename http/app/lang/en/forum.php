4f389734d30f07650804fe09fbc562baed9fce69
Corey Frenette <corey@narwhunderful.com>
2014-04-03 09:16:13 -0300

    Lang

--- http/app/lang/en/forum.php
+++ http/app/lang/en/forum.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-  'topic.link' => HTML::linkAction('forum.view', ':title', [':category', ':topic']) . '<br /><cite class="post-citation">by :user on :date</cite>'
+  'name' => 'Forum',
+  'title' => 'Title',
+  'replies' => 'Replies',
+  'newestpost' => 'Newest Post',
+  'new' => 'New Post',
+  'topic.link' => HTML::linkAction('forum.view', ':title', [':category', ':topic']) . '<br /><cite class="post-citation">by :user on :date</cite>',
+  'topic.newestpost' => '<cite class="post-citation">by :user on :date</cite>'
 ];
