314af86386c01d29e8c54cf34ed93fc83138ac4a
Corey Frenette <corey@narwhunderful.com>
2014-04-05 14:46:32 -0300

    Switch to layout for view topic

--- http/app/views/forum/topic/view.blade.php
+++ http/app/views/forum/topic/view.blade.php
@@ -1,36 +1,26 @@
-<!DOCTYPE html>
+@extends('forum.layout')
 
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>{{ Lang::get('app.title') }} - {{ Lang::get('forum.viewpost.title') }}</title>
-    {{ HTML::style('assets/css/main.css') }}
-    {{ HTML::style('assets/css/forum.css') }}
-    {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js') }}
-  </head>
-  
-  <body>
-    <section>
-      <div class="pure-menu pure-menu-open pure-menu-horizontal">
-        <ul>
+@section('title')
+@lang('forum.title')
+@stop
+
+@section('breadcrumbs')
           <li>{{ HTML::linkAction('forum.index', Lang::get('forum.index')) }}</li>
           <li>></li>
           <li>{{ HTML::linkAction('forum.category', $category->name, $category->path) }}</li>
           <li>></li>
-          @foreach($forums as $f)
+  @foreach($forums as $f)
           <li>{{ HTML::linkAction('forum.view', $f->name, [$category->path, $f->path]) }}</li>
           <li>></li>
-          @endforeach
+  @endforeach
           <li>{{ HTML::linkAction('forum.view', $topic->title, [$category->path, $topic->path]) }}</li>
           <li>></li>
-        </ul>
-      </div>
-    </section>
-    
-    <section>
+@stop
+
+@section('body')
       <table class="forum pure-table pure-table-horizontal pure-table-striped">
         <tbody>
-          @foreach($topic->posts as $post)
+  @foreach($topic->posts as $post)
           <tr>
             <td class="post-id">
               {{{ $post->author->name }}}<br />
@@ -42,11 +32,9 @@
               {{{ $post->body }}}
             </td>
           </tr>
-          @endforeach
+  @endforeach
         </tbody>
       </table>
       
       {{ HTML::linkAction('forum.view', Lang::get('forum.reply'), [$category->path, $topic->path . '/reply']) }}
-    </section>
-  </body>
-</html>
+@stop
