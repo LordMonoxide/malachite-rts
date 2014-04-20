19ce918acd1eb7a1827f5f367c2791de5624a356
Corey Frenette <corey@narwhunderful.com>
2014-04-20 11:49:58 -0300

    Tech language

---
+++ http/app/lang/en/tech.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'title' => 'Technology',
+  'buildings' => 'Buildings',
+  'research' => 'Research',
+  'nodescription' => 'No description',
+  'requirements' => 'Requirements'
+];
