5417807dfee4309b77f73467c292d6c3a78c1022
Corey Frenette <corey@narwhunderful.com>
2014-04-30 08:30:12 -0300

    Oops

--- client/client/src/malachite/api/models/Building.java
+++ client/client/src/malachite/api/models/Building.java
@@ -4,7 +4,7 @@ public class Building {
   public static final String DB_ID   = "id";   //$NON-NLS-1$
   public static final String DB_NAME = "name"; //$NON-NLS-1$
   public static final String DB_TYPE = "type"; //$NON-NLS-1$
-  public static final String DB_TYPE_CAMP       = "camp"; //$NON-NLS-1$
+  public static final String DB_TYPE_BASE       = "base"; //$NON-NLS-1$
   public static final String DB_TYPE_FOODSTORE  = "foodstore"; //$NON-NLS-1$
   public static final String DB_TYPE_WOODSTORE  = "woodstore"; //$NON-NLS-1$
   public static final String DB_TYPE_METALSTORE = "metalstore"; //$NON-NLS-1$
@@ -25,11 +25,11 @@ public class Building {
   }
   
   public enum TYPE {
-    CAMP, FOODSTORE, WOODSTORE, METALSTORE, HOUSING;
+    BASE, FOODSTORE, WOODSTORE, METALSTORE, HOUSING;
     
     public static TYPE fromString(String name) {
       switch(name) {
-        case DB_TYPE_CAMP:       return CAMP;
+        case DB_TYPE_BASE:       return BASE;
         case DB_TYPE_FOODSTORE:  return FOODSTORE;
         case DB_TYPE_WOODSTORE:  return WOODSTORE;
         case DB_TYPE_METALSTORE: return METALSTORE;
