fb855e2c8749bf2693da8cfc3420fa1031350daf
Corey Frenette <corey@narwhunderful.com>
2014-04-30 14:16:25 -0300

    Pathfinding

---
+++ client/client/src/malachite/pathfinding/Pathfinder.java
@@ -0,0 +1,7 @@
+package malachite.pathfinding;
+
+public class Pathfinder {
+  public Point[] findPath(Point start, Point end) {
+    return new Point[] {end};
+  }
+}
---
+++ client/client/src/malachite/pathfinding/Point.java
@@ -0,0 +1,10 @@
+package malachite.pathfinding;
+
+public class Point {
+  public final float x, y;
+  
+  public Point(float x, float y) {
+    this.x = x;
+    this.y = y;
+  }
+}
