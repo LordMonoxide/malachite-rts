17c3464a3c48f230022bccceeb04d5aee66878a6
Corey Frenette <corey@narwhunderful.com>
2014-05-13 17:37:03 -0300

    Order pushes newest first

--- http/app/models/github/Push.php
+++ http/app/models/github/Push.php
@@ -8,4 +8,8 @@ class Push extends Eloquent {
   public function commits() {
     return $this->hasMany('github\Commit');
   }
+  
+  public function scopeNewest($query) {
+    return $query->orderBy('updated_at', 'DESC');
+  }
 }
